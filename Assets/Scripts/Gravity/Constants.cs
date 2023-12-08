6278842d3c5768f2076b1fb193275bac75188c6e
TomRuisseau <tom.ruisseau.300302@gmail.com>
2023-12-08 17:50:34 +0100

    Naive method with trail

---
+++ Assets/Scripts/Gravity/Constants.cs
@@ -0,0 +1,7 @@
+namespace Gravity
+{
+    internal static class Constants {
+        public const float GravitationalConstant = 0.0001f;
+        public const float PhysicsTimeStep = 0.1f;
+    }
+}
