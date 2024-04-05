92c47b0310c9ba941a1a99253fe8e7205672b1b4
Adel <adeleuta@gmail.com>
2024-04-05 17:06:45 +0200

    scene mvts de base etc

---
+++ Assets/Scripts/_PlayerMouvement.cs
@@ -0,0 +1,109 @@
+// Mettre ce script sur le GameObject Player
+// 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class _PlayerMouvement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    private Animator anim;
+
+    private float coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
+
+    private float jumpBufferTime = 0.2f;
+    private float jumpBufferCounter;
+
+    private float horizontal;
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float jumpForce = 16f;
+    private bool isFacingRight = true;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rb.freezeRotation = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        if(horizontal > 0f && !isFacingRight)
+        {
+            Flip();
+        }
+        else if(horizontal < 0f && isFacingRight)
+        {
+            Flip();
+        }
+        if(IsGrounded())
+        {
+            coyoteTimeCounter = coyoteTime;
+            if (jumpBufferCounter > 0)
+            {
+                PerformJump();
+                jumpBufferCounter = 0; 
+            }
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        if (jumpBufferCounter > 0)
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+        
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            if (coyoteTimeCounter > 0f)
+            {
+                PerformJump();
+            }
+            else
+            {
+                jumpBufferCounter = jumpBufferTime;
+            }
+        }
+        if(context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            coyoteTimeCounter = 0f;
+        }
+    }
+    private void PerformJump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        coyoteTimeCounter = 0; 
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+}
+
