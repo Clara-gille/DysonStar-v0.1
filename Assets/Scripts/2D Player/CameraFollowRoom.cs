8d455026a0de896c2747d7057dbf550d71d46e72
Lucas Chanat <calunachat@gmail.com>
2024-04-15 17:47:35 +0200

    camera jungle
    script camera in the jungle

---
+++ Assets/Scripts/2D Player/CameraFollowRoom.cs
@@ -0,0 +1,9 @@
+public Transform camTarget;
+	
+    void OnTriggerEnter2D(Collider2D other) {
+	if (other.tag == "Player") {
+        Camera.main.transform.position = transform.position + new Vector3(0,0,-20);
+        
+    }
+
+    }
