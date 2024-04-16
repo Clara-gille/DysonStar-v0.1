f08831f93b528f75d00dad40496dfb412a4b668c
Ommellete <romain.altmayer@student.junia.com>
2024-04-16 10:03:58 +0200

    optimisation dernier script elevator

--- Assets/Scripts/Ship Events/TriggerElevator.cs
+++ Assets/Scripts/Ship Events/TriggerElevator.cs
@@ -1,23 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TriggerElevator : MonoBehaviour
 {
     ElevatingPlatform elevator;
     private bool OnElevator = false;
+    [SerializeField] private InputActionReference move;
+
+    private void OnEnable()
+    {
+        move.action.Enable();
+        move.action.performed += moveElevator;
+    }
+
+    private void OnDisable()
+    {
+        move.action.Disable();
+        move.action.performed -= moveElevator;
+    }
 
     private void Start()
     {
         elevator = GetComponentInParent<ElevatingPlatform>();
     }
 
-    private void Update()
+    private void moveElevator(InputAction.CallbackContext context)
     {
         if (OnElevator && !elevator.canMove)
         {
             //If the player is in the trigger zone and presses the "Z" key, the elevator will start moving upward if it can
-            if (Input.GetKeyDown(KeyCode.UpArrow))
+            if (context.ReadValue<Vector2>().y > 0)
             {
                 elevator.reverse = false;
                 if (elevator.destinationPoint != elevator.points.Length - 1)
@@ -35,14 +49,14 @@ public class TriggerElevator : MonoBehaviour
 
             }
             //If the player is in the trigger zone and presses the "S" key, the elevator will start moving downward if it can
-            if (Input.GetKeyDown(KeyCode.DownArrow))
+            else if (context.ReadValue<Vector2>().y < 0)
             {
                 elevator.reverse = true;
                 if (elevator.destinationPoint != 0)
                 {
                     elevator.destinationPoint--;
                     foreach (GameObject trigger in elevator.TriggerToDeactivate)
-                    { 
+                    {
                         trigger.SetActive(false);
                     }
                     elevator.TriggerStatus = false;
@@ -51,6 +65,7 @@ public class TriggerElevator : MonoBehaviour
             }
         }
     }
+   
 
     private void OnTriggerEnter2D(Collider2D other)
     {
