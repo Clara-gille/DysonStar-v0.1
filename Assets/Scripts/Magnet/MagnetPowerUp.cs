2617f9774a9a89900727a7b350db082858b124dc
TomRuisseau <tom.ruisseau.300302@gmail.com>
2024-04-16 18:24:29 +0200

    magnet power up

---
+++ Assets/Scripts/Magnet/MagnetPowerUp.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetPowerUp : MonoBehaviour
+{
+    [SerializeField] private GameObject magnetArm;
+    
+    // Detect when there's a collision with a trigger
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) // If player collides with the powerup
+        {
+            // Enable the magnet arm
+            magnetArm.SetActive(true);
+            Destroy(gameObject); // Destroy the powerup
+        }
+    }
+}
