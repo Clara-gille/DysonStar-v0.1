9d717356750399c3f6cbf3f7f8064a9679718674
Adel <adeleuta@gmail.com>
2024-04-17 17:01:04 +0200

    uwu ça Fade

--- Assets/Scripts/2D Player/DeathZone.cs
+++ Assets/Scripts/2D Player/DeathZone.cs
@@ -1,13 +1,25 @@
 ﻿using UnityEngine;
-
+using System.Collections;
 public class DeathZone : MonoBehaviour
 {
+    private Transform playerSpawn;
+    private Animator FadeSystem;
+    private void Awake()
+    {
+        playerSpawn = GameObject.FindGameObjectWithTag("PlayerSpawn").transform;
+        FadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            collision.transform.position = GameObject.FindGameObjectWithTag("PlayerSpawn").transform.position;
+            StartCoroutine(ReplacePlayer(collision));
         }
     }
-    
+    private IEnumerator ReplacePlayer(Collider2D collision)
+    {
+        FadeSystem.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1f);
+        collision.transform.position = playerSpawn.position;
+    }
 }
