325f1c39f055943b5162b396f7b106691a62a429
Ommellete <romain.altmayer@student.junia.com>
2024-04-17 13:47:20 +0200

    FIN JUNGLE

---
+++ Assets/Scripts/Jungle/SpawnRock.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpawnRock : MonoBehaviour
+{
+
+    private bool nearbutton;
+    [SerializeField] private GameObject rock;
+    [SerializeField] private GameObject rockSpawnPoint;
+    [SerializeField] private InputActionReference activate;
+
+    private void OnEnable()
+    {
+        activate.action.Enable();
+        activate.action.performed += RockSpawn;
+    }
+
+    private void RockSpawn(InputAction.CallbackContext context)
+    {
+        // If there's already a rock in the scene, delete it
+        if (GameObject.Find("Big Rock(Clone)") && nearbutton)
+        {
+            Destroy(GameObject.Find("Big Rock(Clone)"));
+        }
+        if (context.performed && nearbutton)
+        {
+            Instantiate(rock, rockSpawnPoint.transform.position, Quaternion.identity);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            nearbutton = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            nearbutton = false;
+        }
+    }
+}
