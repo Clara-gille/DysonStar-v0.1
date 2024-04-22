efdbe73264770bd650716254de80cd61c1443fe5
TomRuisseau <tom.ruisseau.300302@gmail.com>
2024-04-22 12:09:50 +0200

    Menu and swarm comments

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,25 +1,25 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Menu : MonoBehaviour
+namespace Menu
 {
-    public void Start()
+    public class Menu : MonoBehaviour
     {
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
-    }
+        public void Start()
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
 
-    public void StartGame()
-    {
-        // Load the game scene
-        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
-    }
+        public void StartGame()
+        {
+            // Load the game scene
+            UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+        }
 
-    public void QuitGame()
-    {
-        // Quit the game
-        Application.Quit();
+        public void QuitGame()
+        {
+            // Quit the game
+            Application.Quit();
+        }
     }
 }
--- Assets/Scripts/Menu/Options.cs
+++ Assets/Scripts/Menu/Options.cs
@@ -1,34 +1,34 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
-public class Options : MonoBehaviour
+namespace Menu
 {
-    [SerializeField] private AudioMixer audioMixer;
-    [SerializeField] Slider volumeSlider;
-    [SerializeField] Toggle fullscreenToggle;
-
-    public void Start()
+    public class Options : MonoBehaviour
     {
-         volumeSlider.value = PlayerPrefs.GetFloat("Volume", 0);
-         fullscreenToggle.isOn = PlayerPrefs.GetInt("Fullscreen", 1) == 1;
+        [SerializeField] private AudioMixer audioMixer;
+        [SerializeField] Slider volumeSlider;
+        [SerializeField] Toggle fullscreenToggle;
+
+        public void Start()
+        {
+            volumeSlider.value = PlayerPrefs.GetFloat("Volume", 0);
+            fullscreenToggle.isOn = PlayerPrefs.GetInt("Fullscreen", 1) == 1;
          
-    }
+        }
 
-    public void SetVolume(float volume)
-    {
-        float newVolume = volume == -20 ? -80 : volume;
-        audioMixer.SetFloat("Volume", newVolume);
-        PlayerPrefs.SetFloat("Volume", newVolume);
-    }
+        public void SetVolume(float volume)
+        {
+            float newVolume = volume == -20 ? -80 : volume;
+            audioMixer.SetFloat("Volume", newVolume);
+            PlayerPrefs.SetFloat("Volume", newVolume);
+        }
     
-    public void ToggleFullscreen(bool isFullscreen)
-    {
-        Screen.fullScreen = isFullscreen;
-        PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
-    }
+        public void ToggleFullscreen(bool isFullscreen)
+        {
+            Screen.fullScreen = isFullscreen;
+            PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
+        }
     
+    }
 }
